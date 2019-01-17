fec75c3764af88adc0bbd7f20ee278ea8da25ef9
anne-sophie tanguy <anne-sophie.tanguy@epitech.eu>
2019-01-17 16:52:44 -0500

    [ADD] - menu, not responsive

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,18 +1,25 @@
 <template>
     <nav>
-        <ul>
-            <li>
-                <router-link to="/">Home</router-link>
-            </li>
-            <li>
-              <router-link to="/new">New</router-link>
-            </li>
-            <li>
-                <router-link to="/about">About</router-link>
-            </li>
-        </ul>
+      <div class="nav-wrapper">
+        <div class="nav-title"><h1>Caturday</h1></div>
+
+        <div class="nav">
+          <ul>
+              <li>
+                  <router-link to="/">Home</router-link>
+              </li>
+              <li>
+                <router-link to="/new">New</router-link>
+              </li>
+              <li>
+                  <router-link to="/about">About</router-link>
+              </li>
+          </ul>
+        </div>
+      </div>
     </nav>
 </template>
 
 <style>
+  @import "../assets/css/navigation.css";
 </style>
