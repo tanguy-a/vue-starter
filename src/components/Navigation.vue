59e768c0b3ba016620253cce075b66f8c1327bfc
tanguy_a <astanguy@vente-privee.com>
2019-01-17 19:20:22 -0500

    [ADD]: menu side when media

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,7 +4,17 @@
         <div class="nav-title"><h1>Caturday</h1></div>
 
         <div class="nav">
-          <ul>
+
+          <label for="show-menu" class="show-menu">Menu</label>
+          <input type="checkbox" id="show-menu" role="button">
+
+          <div class="container" onclick="myFunction(this)">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+          </div>
+
+          <ul id="menu" class="menu-list">
               <li>
                   <router-link to="/">Home</router-link>
               </li>
